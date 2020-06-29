fd4c5a8c3f6ba983d4e0a1b3f106ca72341519af
arisa <47748473+ali0ikechi@users.noreply.github.com>
2020-06-29 15:40:35 +0200

    Updated on forecast section and weather icon

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import ForecastPreview from "./ForecastPreview.js";
+import axios from "axios";
 
 import "./Forecast.css";
 
-export default function Forecast() {
-  return <div className="Forecast"></div>;
+export default function Forecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function handleForecastResponse(response) {
+    setForecast(response.data);
+    setLoaded(true);
+  }
+
+  if (loaded && props.city === forecast.city.name) {
+    return (
+      <div className="Forecast">
+        <h3>Hourly</h3>
+        <hr />
+        <div className="weather-forecast row">
+          {forecast.list.slice(0, 6).map(function (forecastItem) {
+            return <ForecastPreview data={forecastItem} />;
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "0e8411b87ebdb4c7a51d12f84927e60d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleForecastResponse);
+
+    return null;
+  }
 }
---
+++ src/ForecastPreview.js
@@ -0,0 +1,24 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon.js";
+
+export default function ForecastPreview(props) {
+  function hours() {
+    let date = new Date(props.data.dt * 1000);
+    let hours = date.getHours();
+    return `${hours}:00`;
+  }
+
+  function temperature() {
+    let temperature = Math.round(props.data.main.temp);
+
+    return `${temperature}°C`;
+  }
+
+  return (
+    <div className="ForecastPreview col-2">
+      {hours()}
+      <WeatherIcon code={props.data.weather[0].icon} />
+      {temperature()}
+    </div>
+  );
+}
